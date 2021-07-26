b8a60533ba0a5242fa4af80eb64f97007cb7f34b
BenFischer001 <benfischer52@gmail.com>
2021-07-26 07:45:09 -0500

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AddCat from './src/AddCat';
+
 export default function App() {
+  const [cats,setCats] = useState([{name: 'Life', id: 1}]);
+
+
+  //have use effect to add cat using time for id, have maping inside displayCats 
+
   return (
     <View style={styles.container}>
+      <AddCat setCats={setCats} cats={cats}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
